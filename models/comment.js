dfbf328da50543a4e75306b4ecd0db6aab54293d
mlkrauz <AnElusiveEntity@gmail.com>
2021-11-26 00:08:59 -0500

    Completed models.

---
+++ models/comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+)
+
+module.exports = Comment
